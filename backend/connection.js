c3853a65ebccf91bac7c29ba88d855165b2aa2c0
aswinkumar <mynameisaswin321@gmail.com>
2023-01-20 19:23:40 +0530

    project finished

--- backend/connection.js
+++ backend/connection.js
@@ -3,7 +3,7 @@ const state = {
     db:null
 }
 module.exports.connect=function(done){
-    const url = '"mongodb://0.0.0.0:27017/registration"'
+    const url = "mongodb://0.0.0.0:27017/registration"
     const dbname = 'register'
     mongoClient.connect(url,(err,data)=>{
         console.log('hereeee')
